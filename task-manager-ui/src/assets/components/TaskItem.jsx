b330c79fb3d83e9c12feea42625c61b9dcb1c591
Amcalstrings <ameducaleb@gmail.com>
2024-12-10 09:59:40 +0100

    feat: add frontend implementation and integrate with backend
    - Added React frontend for the application
    - Integrated frontend with existing backend API
    - Updated features include task creation, filtering, and categorization
    - Backend improvements for better API support

---
+++ task-manager-ui/src/assets/components/TaskItem.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react'
+import { UserContext } from '../../App'
+
+const TaskItem = ({task}) => {
+    const { setTasks, } = useContext(UserContext);
+
+    // toggle completed tasks
+    const toggleCompleteTask = async() => {
+        const updatedTask = {...task, completed: !task.completed};
+        await fetch(`http://127.0.0.1:8000/api/tasks/${task.id}`, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(updatedTask),
+        });
+        setTasks((prev) =>
+        prev.map((t) => (t.id === task.id ? {...t, completed: !t.completed} : t)))
+
+    }
+
+    const deleteTask = async() =>{
+        await fetch(`http://127.0.0.1:8000/api/tasks/${task.id}/`, {
+            method: "DELETE",
+        });
+        setTasks((prev) => prev.filter((t) => t.id !== task.id))
+    }
+  return (
+    <div className='flex justify-between items-center border-b py-2'>
+        <div className='flex items-center'>
+            <input 
+            type="checkbox"
+            checked={task.completed}
+            onChange={toggleCompleteTask}
+            className='mr-2'
+            />
+            <span className='task.completed ? line-through : ""'>{task.title}</span>
+        </div>
+        <button onClick={deleteTask} className='text-red-500 hover:underline'>Delete</button>
+    </div>
+  )
+}
+
+export default TaskItem
